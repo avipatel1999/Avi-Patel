fe66aa6e5b6f9d28cdfcc16f8fee9fd2b908a053
avipatel1999 <12255156@cqumail.com>
2024-05-19 20:42:32 +1000

    task 3 done

--- coit20245_prog_fun_ass2_wildlife/sighting.py
+++ coit20245_prog_fun_ass2_wildlife/sighting.py
@@ -1,14 +1,29 @@
-# Task 1 start 
-print("Task 1 Display Menu")
 def display_menu():
 
     print("Menu:")
     print("a. Print help menu")
     print("b. Exit the program")
+    print("c. Display animal species in a city")
+
+def search_species(city):
+
+    return [
+        {"Species": {"TaxonID":"2336","AcceptedCommonName": "dolphin", "PestStatus": "Nil"}},
+        {"Species": {"TaxonID":"655","AcceptedCommonName": "snake", "PestStatus": "Venomous"}}
+    ]
+
+def display_species(species_list):
+  
+    print("Species found:")
+    for species in species_list:
+        name = species["Species"]["AcceptedCommonName"]
+        status = species["Species"]["PestStatus"]
+        Tax = species["Species"]["TaxonID"]
+        print(f"TaxonID: {Tax} , Name: {name}, Pest Status: {status}")
 
 def main():
-    
-    display_menu()  
+   
+    display_menu() 
     while True:
         user_input = input("wildlife> ")
         if user_input == 'help':
@@ -16,8 +31,16 @@ def main():
         elif user_input == 'exit':
             print("Exiting the program.")
             return  
+        elif user_input.startswith('species'):
+            city = user_input.split(maxsplit=1)[1].strip()  
+            species_list = search_species(city)  
+            display_species(species_list)  
         else:
-            print("Invalid command. Please enter 'help' or 'exit'.")
+            print("Invalid command. Please enter 'help', 'exit', or 'species <city>'.")
+
+
 if __name__ == "__main__":
     main()
-# Task 2 End # 
+# Task 3 End #
+# To run your code you want to type this in command 
+# species melbourne # 
