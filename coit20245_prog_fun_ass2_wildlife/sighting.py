0b16c1c915bd86497957c9a372bc256da1eab128
avipatel1999 <12255156@cqumail.com>
2024-05-19 20:34:53 +1000

    main function

--- coit20245_prog_fun_ass2_wildlife/sighting.py
+++ coit20245_prog_fun_ass2_wildlife/sighting.py
@@ -6,5 +6,18 @@ def display_menu():
     print("a. Print help menu")
     print("b. Exit the program")
 
-display_menu()
-# Task 1 End 
+def main():
+    
+    display_menu()  
+    while True:
+        user_input = input("wildlife> ")
+        if user_input == 'help':
+            display_menu()  
+        elif user_input == 'exit':
+            print("Exiting the program.")
+            return  
+        else:
+            print("Invalid command. Please enter 'help' or 'exit'.")
+if __name__ == "__main__":
+    main()
+# Task 2 End # 
